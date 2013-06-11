accf44bcc0a6bba747eadb6e165f41ac84d847f9
Thiago Paes <mrprompt@gmail.com>
2013-06-10 23:11:47 -0300

    chave de testes adicionada

--- sample/resources/config.php
+++ sample/resources/config.php
@@ -1,3 +1,4 @@
 <?php
-define('NUMERO_CIELO', getenv('NUMERO_CIELO'));
-define('CHAVE_CIELO', getenv('CHAVE_CIELO'));
+// par de chaves retirados do exemplo de loja da documentação da cielo.
+define('NUMERO_CIELO', '1001734898');
+define('CHAVE_CIELO', 'e84827130b9837473681c2787007da5914d6359947015a5cdb2b8843db0fa832');
