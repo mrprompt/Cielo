77c6d1e99bcbeb3ca6b04a33dce4195e81ffe896
Thiago Paes <mrprompt@gmail.com>
2013-06-11 01:53:31 -0300

    aplicando um content-type para o caso dos exemplos serem mostrados no navegador

--- sample/resources/config.php
+++ sample/resources/config.php
@@ -1,4 +1,6 @@
 <?php
+header('Content-type:text/plain;encoding=utf-8');
+
 // par de chaves retirados do exemplo de loja da documentação da cielo.
 define('NUMERO_CIELO', '1001734898');
 define('CHAVE_CIELO', 'e84827130b9837473681c2787007da5914d6359947015a5cdb2b8843db0fa832');
