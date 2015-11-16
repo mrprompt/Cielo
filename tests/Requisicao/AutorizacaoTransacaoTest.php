a2e150ca89468d21311c405f4e7072c2849dbd71
Thiago Paes <mrprompt@gmail.com>
2015-11-16 02:12:00 -0200

    docblock

--- tests/Requisicao/AutorizacaoTransacaoTest.php
+++ tests/Requisicao/AutorizacaoTransacaoTest.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * Cielo
+ *
+ * Cliente para o Web Service da Cielo.
+ *
+ * O Web Service permite efetuar vendas com cartões de bandeira
+ * VISA e Mastercard, tanto no débito quanto em compras a vista ou parceladas.
+ *
+ * Licença
+ * Este código fonte está sob a licença GPL-3.0+
+ *
+ * @category   Library
+ * @package    MrPrompt\Cielo\Tests
+ * @subpackage Cliente
+ * @copyright  Thiago Paes <mrprompt@gmail.com> (c) 2013
+ * @license    GPL-3.0+
+ */
 namespace MrPrompt\Cielo\Tests\Requisicao;
 
 use MrPrompt\Cielo\Requisicao\AutorizacaoTransacao;
