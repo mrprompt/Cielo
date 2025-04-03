ac0c207926740e33bbf6ce4b3b28811e10ba1ab8
Thiago Paes <mrprompt@gmail.com>
2025-04-03 16:00:45 -0300

    limpando para receber nova branch

--- sample/resources/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-type:text/plain;encoding=utf-8');
-
-// par de chaves retirados do exemplo de loja da documentação da cielo.
-define('NUMERO_CIELO', '1006993069');
-define('CHAVE_CIELO', '25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033ce2e621b34f3');
